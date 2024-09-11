b41d0c202d3a772da099878201fd60ad0627754f
Harshroxnox <harshroxnox@gmail.com>
2024-09-11 22:56:03 +0530

    setup the qdrant vector database

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# flask-rag
+# Flask-RAG
+## Important Points
+- Make sure to have python3.10, docker and venv installed 
+- create virtual env and activate it 
+- Install from requirements.txt
+- run download.sh
+- run qdrant 
+```bash
+docker pull qdrant/qdrant
+```
+```bash
+docker run -p 6333:6333 -p 6334:6334 \
+    -v $(pwd)/qdrant_storage:/qdrant/storage:z \
+    qdrant/qdrant
+```
+- run rag.py
--- rag.py
+++ rag.py
@@ -1,7 +1,10 @@
-import pymupdf
-import llama_cpp
-import time
+from qdrant_client.models import Distance, VectorParams, PointStruct
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from qdrant_client import QdrantClient
+import llama_cpp
+import pymupdf
+import time
+import uuid
 
 text = ""
 # Open a document
@@ -11,9 +14,6 @@ doc = pymupdf.open("./pdf/2310.06825v1.pdf")
 for page in doc:
     result = page.get_text()
     text += result
-
-# Delete document
-del doc
 
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=300,
@@ -34,4 +34,57 @@ embedding_model = llama_cpp.Llama(
     verbose=False
 )
 
+start = time.time()
+document_embeddings = []
+# Generate Embeddings for every single document in documents and append it into document_embeddings
+for document in documents:
+    embeddings = embedding_model.create_embedding([document.page_content])
+    document_embeddings.extend([
+        (document, embeddings["data"][0]["embedding"])
+    ])
 
+end = time.time()
+all_text = [item.page_content for item in documents]
+char_per_sec = len(''.join(all_text))/ (end-start)
+print(f"TIME: {end-start:.2f} seconds / {char_per_sec:,.2f} chars/second")
+
+print(document_embeddings[0])
+
+client = QdrantClient(path="embeddings")
+
+# If collection VectorDB exists then delete
+if client.collection_exists(collection_name="VectorDB"):
+    client.delete_collection(collection_name="VectorDB")
+
+client.create_collection(
+    collection_name="VectorDB",
+    vectors_config=VectorParams(size=384, distance=Distance.COSINE),
+)
+
+points = [
+    PointStruct(
+        id=str(uuid.uuid4()),
+        vector=embeddings,
+        payload={
+            "text": document.page_content
+        }
+    )
+    for document, embeddings in document_embeddings
+]
+
+operation_info = client.upsert(
+    collection_name="VectorDB",
+    wait=True,
+    points=points
+)
+print(operation_info)
+
+search_query = "What is this document all about?"
+query_vector = embedding_model.create_embedding(search_query)['data'][0]['embedding']
+search_result = client.search(
+    collection_name="VectorDB",
+    query_vector=query_vector,
+    limit=3
+)
+
+print(search_result)
